bada9b4365a723540963f85be862cca6508c0fe7
Amruta Gogawale <abgogawale@gmail.com>
2021-11-19 09:58:14 +0530

    Refactoring of code

--- src/main/java/FourBallChallenge.java
+++ src/main/java/FourBallChallenge.java
@@ -1,15 +1,13 @@
 import processing.core.PApplet;
 
 public class FourBallChallenge extends PApplet {
-    private static final int WIDTH = 640;
-    private static final int HEIGHT = 480;
-    private static final int DISTANCE_FROM_TOP_FRAME = HEIGHT / 5;
-    private static final int DIAMETER = 10;
-    private static final int SPEED_OF_FRAME = 1;
-    private int distanceFromLeftForBall1 = 0;
-    private int distanceFromLeftForBall2 = 0;
-    private int distanceFromLeftForBall3 = 0;
-    private int distanceFromLeftForBall4 = 0;
+    private static final int WIDTH_OF_FRAME = 640;
+    private static final int HEIGHT_OF_FRAME = 480;
+    private static final int DIAMETER_OF_CIRCLE = 10;
+    private static final int SPEED_OF_BALL = 1;
+    private int distanceFromTopOfFrame;
+    private int distanceFromLeftForBall1,distanceFromLeftForBall2,
+            distanceFromLeftForBall3,distanceFromLeftForBall4;
 
     public static void main(String[] args) {
         PApplet.main("FourBallChallenge",args);
@@ -18,11 +16,16 @@ public class FourBallChallenge extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(WIDTH, HEIGHT);
+        size(WIDTH_OF_FRAME, HEIGHT_OF_FRAME);
     }
 
     @Override
     public void setup() {
+        distanceFromTopOfFrame = HEIGHT_OF_FRAME / 5;
+        distanceFromLeftForBall1 = 0;
+        distanceFromLeftForBall2 = 0;
+        distanceFromLeftForBall3 = 0;
+        distanceFromLeftForBall4 = 0;
     }
 
     @Override
@@ -34,30 +37,29 @@ public class FourBallChallenge extends PApplet {
     }
 
     private void drawBall1() {
-        int distanceFromTopOfFrame = DISTANCE_FROM_TOP_FRAME;
-        int speedOfFrame = SPEED_OF_FRAME;
-        ellipse(distanceFromLeftForBall1,distanceFromTopOfFrame,DIAMETER ,DIAMETER);
-        distanceFromLeftForBall1 = distanceFromLeftForBall1 + speedOfFrame;
+        int speedOfBall = SPEED_OF_BALL;
+        ellipse(distanceFromLeftForBall1,distanceFromTopOfFrame,DIAMETER_OF_CIRCLE ,DIAMETER_OF_CIRCLE);
+        distanceFromLeftForBall1 = distanceFromLeftForBall1 + speedOfBall;
     }
 
     private void drawBall2() {
-        int distanceFromTopOfFrame = 2 * DISTANCE_FROM_TOP_FRAME;
-        int speedOfFrame = 2 * SPEED_OF_FRAME;
-        ellipse(distanceFromLeftForBall2,distanceFromTopOfFrame,DIAMETER ,DIAMETER);
-        distanceFromLeftForBall2 = distanceFromLeftForBall2 + speedOfFrame;
+        int distanceFromTopOfFrameForBall2 = 2 * distanceFromTopOfFrame;
+        int speedOfBall = 2 * SPEED_OF_BALL;
+        ellipse(distanceFromLeftForBall2,distanceFromTopOfFrameForBall2,DIAMETER_OF_CIRCLE ,DIAMETER_OF_CIRCLE);
+        distanceFromLeftForBall2 = distanceFromLeftForBall2 + speedOfBall;
     }
 
     private void drawBall3() {
-        int distanceFromTopOfFrame = 3 * DISTANCE_FROM_TOP_FRAME;
-        int speedOfFrame = 3 * SPEED_OF_FRAME;
-        ellipse(distanceFromLeftForBall3,distanceFromTopOfFrame,DIAMETER ,DIAMETER);
-        distanceFromLeftForBall3 = distanceFromLeftForBall3 + speedOfFrame;
+        int distanceFromTopOfFrameForBall3 = 3 * distanceFromTopOfFrame;
+        int speedOfBall = 3 * SPEED_OF_BALL;
+        ellipse(distanceFromLeftForBall3,distanceFromTopOfFrameForBall3,DIAMETER_OF_CIRCLE ,DIAMETER_OF_CIRCLE);
+        distanceFromLeftForBall3 = distanceFromLeftForBall3 + speedOfBall;
     }
 
     private void drawBall4() {
-        int distanceFromTopOfFrame = 4 * DISTANCE_FROM_TOP_FRAME;
-        int speedOfFrame = 4 * SPEED_OF_FRAME;
-        ellipse(distanceFromLeftForBall4,distanceFromTopOfFrame,DIAMETER ,DIAMETER);
-        distanceFromLeftForBall4 = distanceFromLeftForBall4 + speedOfFrame;
+        int distanceFromTopOfFrameForBall4 = 4 * distanceFromTopOfFrame;
+        int speedOfBall = 4 * SPEED_OF_BALL;
+        ellipse(distanceFromLeftForBall4,distanceFromTopOfFrameForBall4,DIAMETER_OF_CIRCLE ,DIAMETER_OF_CIRCLE);
+        distanceFromLeftForBall4 = distanceFromLeftForBall4 + speedOfBall;
     }
 }
